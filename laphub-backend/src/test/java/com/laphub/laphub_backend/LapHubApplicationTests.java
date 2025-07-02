4a2481b7ea613ce1de7f57f2fe97d8f15eb8db5a
siam <mustahsin64@gmail.com>
2025-07-02 13:57:59 +0600

    Initial commit

---
+++ laphub-backend/src/test/java/com/laphub/laphub_backend/LapHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laphub.laphub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
